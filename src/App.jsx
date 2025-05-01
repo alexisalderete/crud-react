5ca4dc27cc96bed900506510d415178bd811298f
alexisalderete <alexis.alderete2002@gmail.com>
2025-05-01 20:52:15 -0300

    no importante

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
             Listar
           </Link>
           <Link className="nav-item nav-link" to={"/new-empleados"}>
-            Agregar nuevooss
+            Agregar nuevoossss
           </Link>
         </div>
       </nav>
